984113869b30706b8135420b183561e9b8578b41
parivesh.thulung <pariveshthulung@gmail.com>
2023-08-28 11:59:45 +0545

    sent post request from postman and save data to database

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// defining requiring libraries
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -11,22 +13,55 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 // set up connection
-mongoose.connect("mongodb://localhost/27017/wikiDB");
+mongoose.connect("mongodb://localhost:27017/wikiDB");
 
-// schema
+// creating schema
 const wikiSchema={
     title: String,
     content: String
 };
 
-// model
+// creating model
 const Article = mongoose.model("article", wikiSchema);
 
+// requesting landing page
+app.get("/",function(req,res){
+    console.log("server started at port 3000.");
+});
+
+// requesting articles page
 app.get("/articles", function(req,res){
-    
+    Article.find()
+    .then(function(data){
+        res.send(data);
+    })
+    .catch(function(err){
+        res.send(err);
+    });
 
 });
 
+// sending data to server
+app.post("/articles",function(req,res){
+
+    // creating document
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    //saving data with err handling
+    newArticle.save()
+    .then(function(){
+        console.log("sucessfully save into database.");
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+});
+
+
+// defining local host
 app.listen(3000,function(){
     console.log("server started in port 3000");
 });
